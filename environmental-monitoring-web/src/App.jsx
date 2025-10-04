3595a2f201771fa0177a86b4bcaaf6166a943b33
Michel Izquierdo <clormichelizquierdor@gmail.com>
2025-10-04 14:44:08 -0500

    Front: Añadiendo la ruta de Alertas al app.jsx

--- environmental-monitoring-web/src/App.jsx
+++ environmental-monitoring-web/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Dashboard from './pages/Dashboard';
 import Sensores from './pages/Sensores';
+import Alertas from "./pages/Alertas"; 
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/sensores" element={<Sensores />} />
+          <Route path="/alertas" element={<Alertas />} />
           {/* Agregar más rutas aquí */}
         </Routes>
       </Layout>
